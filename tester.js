7e4237a7e86c188b571d1fcc1c1d6c00c508ba98
Luca Vicisano <lucavicisano@students.berkeley.net>
2022-04-06 22:35:52 -0700

    idek

--- tester.js
+++ tester.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 let data;
 try{
     data = require('./data.json');
@@ -76,78 +78,61 @@ const allRateCommands = ['autoHigh',
                         'defenseRate'];
 
 let allData = {}
+
 for (const [key, value] of Object.entries(data)){
-    if (allData[key] == null){
+    console.log();
+    console.log();
+    console.log(key + ":")
 
-        allData[key] = {};
+    console.log();
 
-        /*allData[key] = ['all', 'defended', 'notDefended'];
-        for (i in allData[key]){
-            //allData[key][allData[key][i]] = ['totals', 'averages', 'rates'];
-            for (i2 in allTotalCommands){
-                allData[key][allData[key][i]][allData[key][i]['totals']] = allTotalCommands[i2];
-            }
-            for (i2 in allAverageCommands){
-                allData[key][allData[key][i]][allData[key][i]['averages']] = allTotalCommands[i2];
-            }
-            for (i2 in allRateCommands){
-                allData[key][allData[key][i]][allData[key][i]['rates']] = allTotalCommands[i2];
-            }
-        }*/
-    }
+    console.log("\tall:")
+
+    console.log("\t\ttotals:")
     for (i in allTotalCommands){
-        if (allData[key]['all']['totals'][allTotalCommands[i]] == undefined){
-            if (allData[key]['all']['totals'] == undefined){
-                if (allData[key]['all'] == undefined){
-                    allData[key]['all'] = {};
-                }
-                allData[key]['all']['totals'] = {};
-                allData[key]['all']['averages'] = {};
-                allData[key]['all']['rates'] = {};
-            }
-            allData[key]['all']['totals'][allTotalCommands[i]] = {};
-            allData[key]['all']['averages'][allTotalCommands[i]] = {};
-            allData[key]['all']['rates'][allTotalCommands[i]] = {};
-        }
-        allData[key]['all']['totals'][allTotalCommands[i]] = getTotals(allTotalCommands[i], key);
-        allData[key]['defended']['totals'][allTotalCommands[i]] = getDefendedTotals(allTotalCommands[i], key);
-        allData[key]['notDefended']['totals'][allTotalCommands[i]] = getNotDefendedTotals(allTotalCommands[i], key);
+        console.log('\t\t\t' + getTotals(allTotalCommands[i], key))
     }
+    console.log("\t\taverages:")
     for (i in allAverageCommands){
-        if (allData[key]['all']['totals'][allAverageCommands[i]] == undefined){
-            if (allData[key]['all']['totals'] == undefined){
-                if (allData[key]['all'] == undefined){
-                    allData[key]['all'] = {};
-                }
-                allData[key]['all']['totals'] = {};
-                allData[key]['all']['averages'] = {};
-                allData[key]['all']['rates'] = {};
-            }
-            allData[key]['all']['totals'][allAverageCommands[i]] = {};
-            allData[key]['all']['averages'][allAverageCommands[i]] = {};
-            allData[key]['all']['rates'][allAverageCommands[i]] = {};
-        }
-        allData[key]['all']['averages'][allTotalCommands[i]] = getAverages(allTotalCommands[i], key);
-        allData[key]['defended']['averages'][allTotalCommands[i]] = getDefendedAverages(allTotalCommands[i], key);
-        allData[key]['notDefended']['averages'][allTotalCommands[i]] = getNotDefendedAverages(allTotalCommands[i], key);
+        console.log('\t\t\t' + getAverages(allAverageCommands[i], key))
     }
+    console.log("\t\trates:")
     for (i in allRateCommands){
-        if (allData[key]['all']['totals'][allRateCommands[i]] == undefined){
-            if (allData[key]['all']['totals'] == undefined){
-                if (allData[key]['all'] == undefined){
-                    allData[key]['all'] = {};
-                }
-                allData[key]['all']['totals'] = {};
-                allData[key]['all']['averages'] = {};
-                allData[key]['all']['rates'] = {};
-            }
-            allData[key]['all']['totals'][allRateCommands[i]] = {};
-            allData[key]['all']['averages'][allRateCommands[i]] = {};
-            allData[key]['all']['rates'][allRateCommands[i]] = {};
-        }
-        allData[key]['all']['rates'][allTotalCommands[i]] = getRates(allTotalCommands[i], key);
-        allData[key]['defended']['rates'][allTotalCommands[i]] = getDefendedRates(allTotalCommands[i], key);
-        allData[key]['notDefended']['rates'][allTotalCommands[i]] = getNotDefendedRates(allTotalCommands[i], key);
+        console.log('\t\t\t' + getRates(allRateCommands[i], key))
     }
-}
 
+
+
+    console.log("\tdefended:")
+
+    console.log("\t\ttotals:")
+    for (i in allTotalCommands){
+        console.log('\t\t\t' + getDefendedTotals(allTotalCommands[i], key))
+    }
+    console.log("\t\taverages:")
+    for (i in allAverageCommands){
+        console.log('\t\t\t' + getDefendedAverages(allAverageCommands[i], key))
+    }
+    console.log("\t\trates:")
+    for (i in allRateCommands){
+        console.log('\t\t\t' + getDefendedRates(allRateCommands[i], key))
+    }
+
+
+
+    console.log("\tnot:")
+
+    console.log("\t\ttotals:")
+    for (i in allTotalCommands){
+        console.log('\t\t\t' + getNotDefendedTotals(allTotalCommands[i], key))
+    }
+    console.log("\t\taverages:")
+    for (i in allAverageCommands){
+        console.log('\t\t\t' + getNotDefendedAverages(allAverageCommands[i], key))
+    }
+    console.log("\t\trates:")
+    for (i in allRateCommands){
+        console.log('\t\t\t' + getNotDefendedRates(allRateCommands[i], key))
+    }
+    
+}
